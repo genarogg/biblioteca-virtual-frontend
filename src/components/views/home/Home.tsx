fb1f0f64b7745a9eeca407a889bd3f192bc79e89
genaro gonzalez <genarrogg@gmail.com>
2024-05-25 16:06:34 -0400

    se van mostrando las leyendas

--- src/components/views/home/Home.tsx
+++ src/components/views/home/Home.tsx
@@ -1,10 +1,28 @@
 import { useQuery } from "react-query";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 import Layout from "@layout";
 import Spinner from "@spinner";
 
-interface HomeProps {
-
-}
+interface HomeProps {}
 
 const fetcher = async () => {
   const res = await fetch("http://localhost:8000/get-data/estadistica");
@@ -17,12 +35,67 @@ const fetcher = async () => {
 const Home: React.FC<HomeProps> = () => {
   const { data, isLoading, error } = useQuery("fetchData", fetcher);
 
-  if (isLoading) return <Spinner />;
-  // if (error) return <div>Error: {error.message}</div>; // Accede a la propiedad message del objeto de error
+  /*  if (isLoading) return <Spinner />; */
+
+  const labels = [
+    "Trabajo Grado Pregrado",
+    "Producciones Intelectuales Diep",
+    "Publicaciones",
+    "Diplomados Pregrado",
+    "Diplomados Postgrado",
+    "Diplomados Unidades Anexas",
+  ];
+  const chartData = {
+    datasets: [
+      {
+        label: labels[0],
+        data: data?.trabajoGradoPregrado,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: labels[1],
+        data: data?.produccionesIntelectualesDiep,
+        backgroundColor: "rgba(255, 99, 255, 0.5)",
+      },
+      {
+        label: labels[2],
+        data: data?.publicaciones,
+        backgroundColor: "rgba(100, 99, 255, 0.5)",
+      },
+      {
+        label: labels[3],
+        data: data?.diplomadosPregrado,
+        backgroundColor: "rgba(255, 99, 0, 0.5)",
+      },
+      {
+        label: labels[4],
+        data: data?.diplomadosPostgrado,
+        backgroundColor: "rgba(255, 0, 255, 0.5)",
+      },
+      {
+        label: labels[5],
+        data: data?.diplomadosUnidadesAnexas,
+        backgroundColor: "rgba(0, 99, 255, 0.5)",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
 
   return (
     <Layout where="home">
-      <div>{JSON.stringify(data)}</div>
+      <Bar options={options} data={chartData} />
     </Layout>
   );
 };

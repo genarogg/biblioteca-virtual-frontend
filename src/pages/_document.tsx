eb2eef293e625bf9c3dcf5b30af6ad246e6636f3
genaro gonzalez <genarrogg@gmail.com>
2024-06-27 22:25:37 -0400

    terminado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&family=Poppins:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <title>UNERG | Trabajos</title>
+        <meta name="description" content="Trabajos de la UNERG" />
+        <meta name="author" content="Genaro Gonzalez" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="UNERG, Trabajos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/img/favico.jpg" />
       </Head>
       <body>
         <Main />

dbb7b73acb67950417e6e8453c2253a4e072b620
Genaro Gonzalez <genarrogg@gmail.com>
2024-07-25 12:58:13 -0400

    db

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 /* const STRAPI_API_URL="http://localhost:1337" */
-const STRAPI_API_URL = "https://biblioteca-virtual-cms-9wqc.onrender.com";
+const STRAPI_API_URL = "https://cms-trabajos-unerg.onrender.com";
 const STRAPI_TOKEN =
   "2cdc3391c29d5d86578e62d19ce8fee669f45db0f3ace43becfdf722415d0fbf67a4db19debfc9e38dd73a9d2c3162ea26147437bdd25ec14bd599f7920c716d7ab4305c8925a95c67d9a4fab08eff350fb2854200c81b5da220c91eb59aee1a591394c461ce4007da61d25cb2ebdd69abe6dcbc1bd71e87a697e42ddcd54ac4";
 
